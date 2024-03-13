66f9c34ff8784a4b92187b5ccbb6afd490a8ffcb
SaikatKarar <kararsaikat@gmail.com>
2024-03-13 12:40:10 +0530

    Add SIDEBAR

--- assets/js/script.js
+++ assets/js/script.js
@@ -6,3 +6,15 @@ const preloader = document.querySelector("[data-preloader]");
 window.addEventListener("load", () => {
     preloader.classList.add("remove");
 });
+
+/**
+ * #SIDEBAR
+ */
+const sidebar = document.querySelector('.sidebar');
+const body = document.querySelector('body');
+function showSidebar() {
+    sidebar.style.right = 0;
+}
+function closeSidebar() {
+    sidebar.style.right = '-150%';
+}
